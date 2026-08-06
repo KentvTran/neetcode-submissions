d0ce4b0b09acdb70b5118f65b478f47bcf3df250
Kent Tran <111903963+KentvTran@users.noreply.github.com>
2026-08-05 19:55:33 -0700

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,37 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False #flag for end of a valid word
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            curr = curr.children[char]
+        curr.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        def dfs(ix, node):
+            if ix == len(word):
+                return node.endOfWord
+            char = word[ix]
+
+            #unknown char case
+            if char == ".":
+                for child in node.children.values():
+                    if dfs(ix + 1, child):
+                        return True
+                return False
+            #standard char case
+            else:
+                if char not in node.children:
+                    return False
+                return dfs(ix + 1, node.children[char])
+        return dfs(0,self.root)
+        
